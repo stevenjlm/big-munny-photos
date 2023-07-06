de477a8eaf16f991926acbfab8f66298b7832c8b
steven <stevenjmunn@gmail.com>
2023-07-05 21:11:59 -0700

    Setup columns and more imgs

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,21 @@
 import Image from 'next/image'
 import { Tab } from '@headlessui/react'
 
+// for all tab
+import img1 from '../public/all/cablecar.jpg';
+import img2 from '../public/all/me_portrait.jpg';
+import img3 from '../public/all/ciel.jpg';
+import img4 from '../public/all/sf_rainbow.jpg';
+import img5 from '../public/all/pelican.jpg';
+import img6 from '../public/all/sb.jpg';
+import img7 from '../public/all/nissan.jpg';
+import img8 from '../public/all/portrait1.jpg';
+import img9 from '../public/all/yane.jpg';
+import img10 from '../public/all/saori.jpg';
+import img11 from '../public/all/cars.jpg';
+import img12 from '../public/all/alaska.jpg';
+
+
 
 export default function Home() {
   return (
@@ -12,7 +27,7 @@ export default function Home() {
         <button className='rounded-xl bg-white text-slate-950 text-4xl font-medium px-5 py-2 hover:bg-emerald-800 '>Contact</button>
       </header>
       <main>
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center mx-12">
           <Tab.Group>
           <Tab.List className="flex items-center gap-4 py-0 text-slate-200">
             <Tab>All</Tab>
@@ -23,20 +38,12 @@ export default function Home() {
           </Tab.List>
           <Tab.Panels>
             <Tab.Panel className="py-4">
-            <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-              {FirstColumn()}
-                  <div className="grid gap-4">
-                      <div>
-                          <img className="h-auto max-w-full rounded-s" src="/IMG_1134-Edit.jpg" alt="Sunset along Mendocino coast"/>
-                      </div>
-                      <div>
-                          <img className="h-auto max-w-full rounded-s" src="/IMG_5795-Edit.jpg" alt="Night photo of Fushimi Inari"/>
-                      </div>
-                      <div>
-                          <img className="h-auto max-w-full rounded-s" src="/IMG_8189-Edit.jpg" alt="Sunset along Santa Barbara coast"/>
-                      </div>
-                  </div>
-              </div>
+            <div className="grid grid-cols-2 md:grid-cols-4 sm:grid-cols-1 gap-4">
+              {ImageColumn(img1, img2, img3)}
+              {ImageColumn(img4, img5, img6)}
+              {ImageColumn(img7, img8, img9)}
+              {ImageColumn(img10, img11, img12)}
+            </div>
             </Tab.Panel>
             <Tab.Panel>Content 2</Tab.Panel>
             <Tab.Panel>Content 3</Tab.Panel>
@@ -49,17 +56,17 @@ export default function Home() {
 }
 
 
-export function FirstColumn() {
+export function ImageColumn(firstImg: any, secondImg: any, thirdImg: any) {
   return(
     <div className="grid gap-4">
         <div>
-            <img className="h-auto max-w-full rounded-s" src="/CableCar-8by12.jpg" alt="Cable car in San Francisco"/>
+            <Image src={firstImg} alt=" "/>
         </div>
         <div>
-            <img className="h-auto max-w-full rounded-s" src="/IMG_2425-Edit.jpg" alt="Drift car at night"/>
+            <Image src={secondImg} alt=" "/>
         </div>
         <div>
-            <img className="h-auto max-w-full rounded-s" src="/IMG_8368-Edit.jpg" alt="Two Pelicans diving"/>
+            <Image src={thirdImg} alt=" "/>
         </div>
     </div>
   )
